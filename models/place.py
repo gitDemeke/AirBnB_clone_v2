6fdd2899a6b7a0156a4c68d9c0b859d15448df71
gitDemeke <demeke.asres@aau.edu.et>
2023-04-21 18:48:13 +0300

    web  statis. deploy ad falsk

---
+++ models/place.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
+
+association_table = Table('place_amenity', Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True,
+            nullable=False))
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False, backref="amenities")
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """Returns the list of Review instances"""
+            return [review for review in self.reviews]
+
+        @property
+        def amenities(self):
+            """Returns a list of Amenity instances"""
+            return [amenity for amenity in self.amenities]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Appends an amenity id to the attribute
+            amenity_id
+            """
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
