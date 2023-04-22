f75e59afb61d1352225fbed6c6ecb0370917ec73
gitDemeke <demeke.asres@aau.edu.et>
2023-04-22 17:11:03 +0300

    Web Flask

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
--- Setup for MySQL server
--- Script to automate setup
+-- creates database and user
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE USER If NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
